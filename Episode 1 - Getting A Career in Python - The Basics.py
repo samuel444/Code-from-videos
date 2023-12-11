674b4739fc14171f998420491e299ca614853852
samuel444 <sam@samuelw.me>
2023-12-11 19:44:50 +0000

    Update Episode 1 - Getting A Career in Python - The Basics.py

--- Episode 1 - Getting A Career in Python - The Basics.py
+++ Episode 1 - Getting A Career in Python - The Basics.py
@@ -1,71 +1,102 @@
+# The Print Function
 print ('Hello World!')
 
+# Variables
+#Integers
 num = 7
 
+# Floats
 deci = 4.9
 
+# Strings
 colour = "yellow"
 
+# Booleans
 decision = True
 
+# Printing Variables
 print(num)
 print(deci)
 print(colour)
 print(decision)
 
+# Operations
+# Arithmetic
+# Adding
 result = 5 + 3
 print(result)
 
+# Subtracting
 result = 7 - 2
 print(result)
 
+# Multiplying
 result = 4 * 7
 print(result)
 
+# Dividing
 result = 5 / 2
 print(result)
 
+# Floor Division
 result = 5 // 2
 print(result)
 
+# Modulus
 result = 12 % 5
 print(result)
 
+# Exponential
 result = 7 ** 2
 print(result)
 
+# Comparison
+# Equivalence
 print(7==7)
 
 print(6==7)
 
+# Not Equals
 print(7!=7)
 
 print(6!=7)
 
+# Greater Than
 print(8>6)
 
+# Less Than
 print(6<9)
 
+# Greater/Less Than or Equals
 print(6>=6)
 
 print(6<=6)
 
+# Logical
+# And
 print(True and False)
 print(True and True)
 print(False and False)
 
+# Or
 print(True or False)
 print(True or True)
 print(False or False)
 
+# Not
 print(not True)
 print(not False)
 
+# String Concatenation
+# The Variables Used:
 string1 = "Hello"
 string2 = "World"
 
+# Using a Plus '+'
 print(string1 + string2)
 
+# Using a Comma ','
 print(string1,string2)
 
+# Comma transform other data types to strings:
 print(string1,5)
