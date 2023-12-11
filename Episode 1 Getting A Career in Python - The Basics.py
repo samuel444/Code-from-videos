b7cd488fc3b2498ef8113bd8a61afefd32f5c27b
samuel444 <sam@samuelw.me>
2023-12-11 19:36:01 +0000

    Create Episode 1 Getting A Career in Python - The Basics.py

---
+++ Episode 1 Getting A Career in Python - The Basics.py
@@ -0,0 +1,71 @@
+print ('Hello World!')
+
+num = 7
+
+deci = 4.9
+
+colour = "yellow"
+
+decision = True
+
+print(num)
+print(deci)
+print(colour)
+print(decision)
+
+result = 5 + 3
+print(result)
+
+result = 7 - 2
+print(result)
+
+result = 4 * 7
+print(result)
+
+result = 5 / 2
+print(result)
+
+result = 5 // 2
+print(result)
+
+result = 12 % 5
+print(result)
+
+result = 7 ** 2
+print(result)
+
+print(7==7)
+
+print(6==7)
+
+print(7!=7)
+
+print(6!=7)
+
+print(8>6)
+
+print(6<9)
+
+print(6>=6)
+
+print(6<=6)
+
+print(True and False)
+print(True and True)
+print(False and False)
+
+print(True or False)
+print(True or True)
+print(False or False)
+
+print(not True)
+print(not False)
+
+string1 = "Hello"
+string2 = "World"
+
+print(string1 + string2)
+
+print(string1,string2)
+
+print(string1,5)
