61741c698b5dbc27adff75fc0e819fdbabf3ba74
samuel444 <samjw444@gmail.com>
2024-10-24 14:22:51 +0100

    Add files via upload

---
+++ Python/Lambda.py
@@ -0,0 +1,13 @@
+example1 = lambda : print ('Hello')
+example1()
+example2 = lambda x, y : x + y
+print (example2(2,5))
+example3 = lambda x, y, z: x + y - z
+print (example3(y = 4, z = 2, x = 3))
+example4 = lambda *args : sum(args)
+print (example4(5, 2, 5, 7, 6))
+list1 = [14, 1, 8, 3, 9, 12, 5, 2, 71]
+example5 = list(map(lambda x : x+2, list1))
+print (example5)
+example6 = list(filter(lambda x : x>5, list1))
+print (example6)
